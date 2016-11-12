73b13afcbc4d467e481d0225ca30b685157f7d50
takahirom <takam.dev@gmail.com>
2016-11-12 13:47:17 +0900

    Add dependencies

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,12 @@
+import io.reactivex.*;
+import javafx.util.Pair;
+
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public static void main(String[] args) throws InterruptedException {
+        Single.zip(Single.just(1), Single.just(1), Pair::new)
+                .test()
+                .assertValue(new Pair(1, 1));
     }
+
 }
 
